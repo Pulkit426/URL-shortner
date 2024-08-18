57b0ed348bae9382a1a14361b0b2521fcbb98e79
Pulkit426 <agrawalpulkit12@gmail.com>
2024-08-18 15:45:51 +0530

    create model for url collection and add post route for url

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const app = express();
 
+const urlRoute = require('./routes/url');
+
+const app = express();
 const PORT = 8000;
+
+app.use(express.json());
+app.use('/url', urlRoute);
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
